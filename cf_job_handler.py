c55e84df6d5ae4bde53a6576b6b734ca777b822f
luigicfh <66189982+luigicfh@users.noreply.github.com>
2022-09-22 16:32:23 -0600

    updates do handlers and moved utlils to handler_cf_v1

--- cf_job_handler.py
+++ cf_job_handler.py
@@ -2,58 +2,18 @@ from google.cloud import firestore
 from handler_cf_v1 import services
 from handler_cf_v1 import apps
 from handler_cf_v1.services import JOB_STATES
+from handler_cf_v1.utils import *
 import traceback
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib
-import ssl
 import os
 import json
 
 ENV_VAR_MSG = "Specified environment variable is not set."
 
 
-def get_doc(db: firestore.Client, collection: str, id: str) -> dict:
-
-    return db.collection(collection).document(id).get().to_dict()
-
-
-def update_doc(db: firestore.Client, collection: str, id: str, doc: dict, state_msg=None) -> dict:
-
-    if state_msg:
-
-        doc['state_msg'] = state_msg
-
-    db.collection(collection).document(id).set(doc)
-
-    return db.collection(collection).document(id).get().to_dict()
-
-
-def send_email(to: list, subject: str, body: str) -> None:
+def notify_error(db, collection, id, doc):
 
     sender = os.environ.get('SENDER', ENV_VAR_MSG)
     password = os.environ.get('PASSWORD', ENV_VAR_MSG)
-
-    message = MIMEMultipart("alternative")
-    message['Subject'] = subject
-    message['From'] = sender
-    message['To'] = ",".join(to)
-
-    part1 = MIMEText(body, "plain")
-    part2 = MIMEText(body, "html")
-
-    message.attach(part1)
-    message.attach(part2)
-
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender, password)
-        server.sendmail(
-            sender, to, message.as_string()
-        )
-
-
-def notify_error(db, collection, id, doc):
 
     doc['state'] = JOB_STATES[3]
     updated_job = update_doc(db, collection, id, doc)
@@ -69,7 +29,7 @@ def notify_error(db, collection, id, doc):
         Request: <pre>{json.dumps(updated_job['request'])}</pre>
     """
 
-    return send_email(recipients, subject, body)
+    return send_email(sender, password, recipients, subject, body)
 
 
 def job_create_handler(data, context):
