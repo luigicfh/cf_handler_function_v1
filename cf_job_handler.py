e5ffb7b8eb22a5c75387da6b80a0915aca754bf9
luigicfh <66189982+luigicfh@users.noreply.github.com>
2022-11-03 23:10:18 -0600

    requirements update

--- cf_job_handler.py
+++ cf_job_handler.py
@@ -11,16 +11,12 @@ ENV_VAR_MSG = "Specified environment variable is not set."
 
 
 def notify_error(db, collection, id, doc):
-
     sender = os.environ.get('SENDER', ENV_VAR_MSG)
     password = os.environ.get('PASSWORD', ENV_VAR_MSG)
-
     doc['state'] = JOB_STATES[3]
     updated_job = update_doc(db, collection, id, doc)
-
     recipients = os.environ.get('RECIPIENTS', ENV_VAR_MSG).split(",")
     subject = f"AT Central Notifications | JOB ERROR ID {id}"
-
     body = f"""
         Job ID: {id}<br>
         <hr>
@@ -28,27 +24,18 @@ def notify_error(db, collection, id, doc):
         Error:  <pre>{updated_job['state_msg']}</pre><br>
         Request: <pre>{json.dumps(updated_job['request'])}</pre>
     """
-
     return send_email(sender, password, recipients, subject, body)
 
 
 def job_create_handler(data, context):
-
     path_parts = context.resource.split('/')
-
     db = firestore.Client(path_parts[1])
-
     job_id = path_parts[-1]
     collection = path_parts[-2]
-
     job = get_doc(db, collection, job_id)
-
     service = getattr(services, job['service_instance']['className'])
-
     app = getattr(apps, job['service_instance']['appClassName'])
-
     instance = service(job['service_instance'], job, app)
-
     try:
         instance.execute_service()
     except Exception as e:
@@ -58,28 +45,19 @@ def job_create_handler(data, context):
 
 
 def job_update_handler(data, context):
-
     if data['value']['fields']['state']['stringValue'] != JOB_STATES[0]:
         return "OK"
-
     path_parts = context.resource.split('/')
-
     db = firestore.Client(path_parts[1])
     collection = path_parts[-2]
     job_id = path_parts[-1]
     job = get_doc(db, collection, job_id)
-
     job['retry_attempt'] = job['retry_attempt'] + 1
-
     if job['retry_attempt'] == 3:
         return notify_error(db, collection, job_id, job)
-
     service = getattr(services, job['service_instance']['className'])
-
     app = getattr(apps, job['service_instance']['appClassName'])
-
     instance = service(job['service_instance'], job, app)
-
     try:
         instance.execute_service()
     except Exception as e:
